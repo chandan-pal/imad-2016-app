a0e87453461dd81292d8fdac45066d90b3f4ce06
chandan-pal <chandanpal290190@gmail.com>
2016-10-15 19:39:14 +0530

    new look

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,15 @@
-$(document).ready(function () {
-	
-	alert('YAY!');
-	
-});
+// Create a request object
+var req = new XMLHttpRequest();
+// Capture the response and store it in a variable
+req.onreadystatechange = function() {
+	if (req.readyState == XMLHttpRequest.DONE) {
+		if (req.status == 200) {
+			var count = req.responseText;
+			var counterSpan = document.getElementById('counter');
+			counterSpan.innerHTML = count;
+		}
+	}
+};
+// Make the request
+req.open('GET', 'http://chandan-pal.imad.hasura-app.io/counter', true);
+req.send(null);
